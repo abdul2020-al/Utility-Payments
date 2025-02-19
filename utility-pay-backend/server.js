a139ec140fcf4fb55ae8cedc28ca2bc05c00c98c
abdul2020-al <abdulalsalkhadi5@gmail.com>
2025-02-19 11:49:48 -0500

    Add files via upload

---
+++ utility-pay-backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Database connected"))
+.catch((err) => console.log(err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
+
+ll
